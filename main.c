88a660da0c7000b7833080fd030446e8d7787307
rep1 <rep1@test.com>
2015-11-08 11:59:27 +0900

    add sub-fucntion & edit main.c

--- main.c
+++ main.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 
 int add(int, int);
+int sub(int, int);
 
 void main(void)
 {
+	int op1 =10;
+	int op2 =5;
+	int ret0;
+	int ret1;
+	
+	ret0 = add(op1, op2);
+	ret1 = sub(op1, op2);
+
 }
 
 int add(int a, int b)
 {
 	return a + b;
+}
+
+int sub(int a, int b)
+{
+	return a - b;
 }
